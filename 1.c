9d2d431a7f830b7e10ede5408a6750fefd3d51af
Ciberth <thomasclauwaert@gmail.com>
2016-10-02 23:34:39 +0200

    Oef 1

--- 1.c
+++ 1.c
@@ -1,5 +1,21 @@
 // Oefening 1
 
+#include<stdio.h>
+#define AANTAL 10
+
+int main(){
+
+	int i;
+	printf("Hello World!\n");
+	for(i=AANTAL; i>0; i--){
+		printf("%d ",i);
+	}
+	printf("\n");	
+	return 0;
+} 
+
+
+
 
 
 
